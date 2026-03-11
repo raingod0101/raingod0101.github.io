4b067e76f98bcb39168c4667d52f1b05e9c0d9e0
raingod0101 <raingod0101@gmail.com>
2026-03-11 19:42:07 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 1. 自動設定網頁分頁欄圖示 (Favicon)
-    let link = document.querySelector("link[rel~='icon']");
+    // 1. 自動設定網頁分頁欄圖示 (Favicon) - 加上版本號避免快取
+    const iconPath = "p4.png"; // 如果你的 p4.png 跟 html 在同一個資料夾，把前面的 / 去掉
+    const version = new Date().getTime(); // 產生一組隨機數字
+    
+    let link = document.querySelector("link[rel*='icon']");
     if (!link) {
         link = document.createElement('link');
-        link.rel = 'icon';
+        link.rel = 'shortcut icon';
         document.getElementsByTagName('head')[0].appendChild(link);
     }
-    link.href = '/p4.png'; // 這裡設定你的 icon 路徑
+    // 強制在網址後面加上 ?v=123456789，瀏覽器就會以為是新檔案而重新下載
+    link.href = `${iconPath}?v=${version}`;
 
     // 2. 載入 FontAwesome 圖示庫
     const fa = document.createElement('link');
