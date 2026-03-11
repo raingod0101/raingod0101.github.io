42753ba1e65cd5b9795654f266be6812b101e28f
raingod0101 <raingod0101@gmail.com>
2026-03-11 19:43:47 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,30 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 1. 自動設定網頁分頁欄圖示 (Favicon) - 加上版本號避免快取
-    const iconPath = "p4.png"; // 如果你的 p4.png 跟 html 在同一個資料夾，把前面的 / 去掉
-    const version = new Date().getTime(); // 產生一組隨機數字
+    // 1. 強力注入 Favicon
+    const iconPath = "p4.png";
+    const v = new Date().getTime(); // 每次刷新都產生新版本號
     
-    let link = document.querySelector("link[rel*='icon']");
-    if (!link) {
-        link = document.createElement('link');
-        link.rel = 'shortcut icon';
-        document.getElementsByTagName('head')[0].appendChild(link);
-    }
-    // 強制在網址後面加上 ?v=123456789，瀏覽器就會以為是新檔案而重新下載
-    link.href = `${iconPath}?v=${version}`;
+    // 移除舊的 icon 標籤
+    const oldIcons = document.querySelectorAll("link[rel*='icon']");
+    oldIcons.forEach(el => el.remove());
 
-    // 2. 載入 FontAwesome 圖示庫
+    // 建立新的 icon 標籤
+    const link = document.createElement('link');
+    link.type = 'image/png';
+    link.rel = 'shortcut icon'; // 為了相容舊版瀏覽器
+    link.href = `${iconPath}?v=${v}`;
+    document.getElementsByTagName('head')[0].appendChild(link);
+
+    // 額外增加一個標準 rel="icon"
+    const link2 = document.createElement('link');
+    link2.type = 'image/png';
+    link2.rel = 'icon';
+    link2.href = `${iconPath}?v=${v}`;
+    document.getElementsByTagName('head')[0].appendChild(link2);
+
+    // 2. 載入 FontAwesome
     const fa = document.createElement('link');
     fa.rel = 'stylesheet';
     fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
     document.head.appendChild(fa);
 
-    // 3. 抓取並注入 menu.html
+    // 3. 注入選單
     const container = document.getElementById('nav_bar');
     if (container) {
         fetch('menu.html')
             .then(res => res.text())
-            .then(data => { 
-                container.innerHTML = data; 
-            });
+            .then(data => { container.innerHTML = data; });
     }
 });
